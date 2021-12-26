ff46868fe4c06d47ec47348e5bf5d6a21b6ca66b
leehj <leehj@scenappsm.com>
2021-12-27 04:14:45 +0900

    saga 리팩토링

--- src/modules/githup/sagas.ts
+++ src/modules/githup/sagas.ts
@@ -1,18 +1,19 @@
 import { getUserProfileAsync } from "./actions";
-import { call, put, takeEvery} from 'redux-saga/effects';
-import { getUserProfile, GithubProfile } from "../../api/github";
+import { takeEvery} from 'redux-saga/effects';
+import { getUserProfile, } from "../../api/github";
 import { GET_USER_PROFILE } from ".";
-import { AxiosError } from "axios";
-function* getUserProfileSaga(action:ReturnType<typeof getUserProfileAsync.request>){
-    try{
-        const userProfile:GithubProfile = yield call(getUserProfile, action.payload);
-        yield put(getUserProfileAsync.success(userProfile));
-    }catch(e){
-        if ((<any>e).isAxiosError) { 
-            yield put(getUserProfileAsync.failure(<AxiosError>e));
-        }
-    }
-}
+import createAsyncSaga from "../../lib/createAsyncSaga";
+
+// function* getUserProfileSaga(action:ReturnType<typeof getUserProfileAsync.request>){
+//     try{
+//         const userProfile:GithubProfile = yield call(getUserProfile, action.payload);
+//         yield put(getUserProfileAsync.success(userProfile));
+//     }catch(e:any){
+//          yield put(getUserProfileAsync.failure(e));
+//     }
+// }
+
+const getUserProfileSaga = createAsyncSaga(getUserProfileAsync, getUserProfile);
 
 export function* githubSaga(){
     yield takeEvery(GET_USER_PROFILE, getUserProfileSaga);
